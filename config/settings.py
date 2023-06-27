849ebabc719c2cb03cf403ca2c84fd354cb03adb
YAMASHITA <jpymst19870624@gmail.com>
2023-06-28 07:18:35 +0900

    Blog complete_4

--- config/settings.py
+++ config/settings.py
@@ -130,10 +130,10 @@ LOGIN_REDIRECT_URL = '/'
 ACCOUNT_LOGOUT_REDIRECT_URL = '/'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
