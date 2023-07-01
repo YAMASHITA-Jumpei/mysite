c71b11186974ee72c6df0e7745560a121786421a
YAMASHITA <jpymst19870624@gmail.com>
2023-07-01 11:42:00 +0900

    About Complete_3

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-01jr9rb-$b%m2xwt6snlh16bh#qt32uch-h*t-vp+msw#jk&vk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '.pythonanywhere.com',
                  'jumpeiyamashita.pythonanywhere.com']
